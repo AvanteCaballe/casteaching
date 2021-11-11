1c4b6b7b0f363a0ad5dc03934b4828eb90948a17
AvanteCaballe <marcavante@iesebre.com>
2021-11-11 20:09:07 +0100

    Resultat video 107. Corecció exercicis proposats i Testos unitaris, Funcions helpers i preparació de la base de dades

--- config/casteaching.php
+++ config/casteaching.php
@@ -3,6 +3,8 @@
 
 return [
     'default_user' => [
+        'name' => env('DEFAULT_USER_NAME','Marc Avante Caballé'),
+        'email' => env('DEFAULT_USER_EMAIL','marcavantecaballe@gmail.com'),
         'password' => env('DEFAULT_USER_PASSWORD','12345678')
     ]
 ];
