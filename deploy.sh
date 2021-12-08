3a31e66eb5a5cf312f0270765794dfe6f0cb17f8
AvanteCaballe <marcavante@iesebre.com>
2021-12-08 17:54:22 +0100

    Arreglat script de deploy

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ git merge main
 git push origin production
 git checkout main
 
-https://forge.laravel.com/servers/506169/sites/1507990/deploy/http?token=6dotl58B10pU9wEn7H5kNZAsI8TZksDgI5fU4YVg
+wget https://forge.laravel.com/servers/506169/sites/1507990/deploy/http?token=6dotl58B10pU9wEn7H5kNZAsI8TZksDgI5fU4YVg
